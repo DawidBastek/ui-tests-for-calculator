79ea5f32be5587afd7ecabd2e92e73b1a53aa194
DawidBastek <72853287+DawidBastek@users.noreply.github.com>
2021-01-27 15:12:59 +0100

    Create AccessibilityIdentifiers.swift

---
+++ Calculator SwiftUIUITests/AccessibilityIdentifiers.swift
@@ -0,0 +1,14 @@
+/**
+ * AccessibilityIdentifiers enum is a place where all variables with names for .accessibility should be placed.
+ Suggested convention:
+    enum AccessibilityIdentifiers {
+        enum SectionA { a b c } }
+        enum SectionB { a b c } }
+    }
+ */
+enum AccessibilityIdentifiers {
+    enum Calculator {
+        static let resultValueText = "resultValueText"
+        static let inputValueText = "inputValueText"
+    }
+}
